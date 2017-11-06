500b881561c0d3dd16bf574087dbf29cf04fb412
lbrande <lovbra00@gmail.com>
2017-11-06 20:53:16 +0100

    Interpreter is working.

--- brainfuck.c
+++ brainfuck.c
@@ -11,10 +11,15 @@ int main(int argc, char const *argv[]) {
 				printf("ERROR: Input file not found!\n");
 				return 2;
 		}
+		fseek(fp, 0, SEEK_END);
+		long fsize = ftell(fp);
+		rewind(fp);
+		char* code = malloc(fsize);
+		fread(code, fsize, 1, fp);
+		fclose(fp);
 		char* ptr = calloc(argc > 2 ? atoi(argv[2]) : 1000, 1);
-		int c;
-		while ((c = fgetc(fp)) != EOF) {
-				switch (c) {
+		for(int c_ptr = 0; c_ptr < fsize; ++c_ptr) {
+				switch (code[c_ptr]) {
 				case '>': ++ptr;
 						break;
 				case '<': --ptr;
@@ -28,8 +33,38 @@ int main(int argc, char const *argv[]) {
 				case ',': *ptr=getchar();
 						break;
 				case '[':
+						if(!*ptr) {
+								int open = 1;
+								while (open > 0 && c_ptr < fsize) {
+										++c_ptr;
+										if (code[c_ptr] == ']') {
+												open--;
+										} else if(code[c_ptr] == '[') {
+												open++;
+										}
+								}
+								if(open) {
+										printf("ERROR: Brackets does not match!\n");
+										return 3;
+								}
+						}
 						break;
 				case ']':
+						if(*ptr) {
+								int open = 1;
+								while (open > 0 && c_ptr >= 0) {
+										--c_ptr;
+										if (code[c_ptr] == '[') {
+												open--;
+										} else if(code[c_ptr] == ']') {
+												open++;
+										}
+								}
+								if(open) {
+										printf("ERROR: Brackets does not match!\n");
+										return 3;
+								}
+						}
 						break;
 				}
 		}
